afe10ae7d2cdf41ef99a5feacba9bb90b32cd7b2
Luc Cary <luc.cary@gmail.com>
2018-03-10 23:08:19 -0500

    isort snippet urls

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import url, include
+from django.conf.urls import include, url
 from rest_framework.routers import DefaultRouter
 from rest_framework.schemas import get_schema_view
+
 from snippets import views
 
 schema_view = get_schema_view(title='Pastebin API')
